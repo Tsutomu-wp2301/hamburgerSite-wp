5ae53058ed5c74545394fc8b6d2f0ac190a8c967
Tsutomu <tutokun0723@gmail.com>
2023-04-16 00:45:01 +0900

    CSSga読み込まない２

--- functions.php
+++ functions.php
@@ -5,12 +5,13 @@
 
 function add_files(){
     wp_enqueue_style('my_style', get_template_directory_uri().'/css/style.css');
-    wp_enqueue_script('my_script', get_template_directory_uri().'/JS/fadeIm.js');
-    wp_enqueue_script('my_script', get_template_directory_uri().'/JS/fadeOut.js');
-    wp_enqueue_script('my_script', get_template_directory_uri().'/JS/toggle-class--close.js');
-    wp_enqueue_script('my_script', get_template_directory_uri().'/JS/toggle-class--open.js');
+    wp_enqueue_script('fadeIn_script', get_template_directory_uri().'/JS/fadeIm.js');
+    wp_enqueue_script('fadeOut_script', get_template_directory_uri().'/JS/fadeOut.js');
+    wp_enqueue_script('toggle_class_close_script', get_template_directory_uri().'/JS/toggle-class--close.js');
+    wp_enqueue_script('toggle_class_open_script', get_template_directory_uri().'/JS/toggle-class--open.js');
 }
 add_action('wp_enqueue_scripts', 'add_files');
+
 
 
 
