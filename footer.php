aa18817310ed56ac60e90d87e4e135414679e626
Tsutomu <tutokun0723@gmail.com>
2023-04-15 09:53:54 +0900

    top2

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer class="l-footer p-footer">
+    <!-- <div class="l-footer__layout">
+      <a href="#"><p>ショップ情報</p></a>
+      <div class="c-line"></div>
+      <a href="#"><p>ヒストリー</p></a>
+    </div> -->
+    <?php wp_nav_menu(); ?>
+    <p class="copyright">Copyright :  RaisTech</p>
+  </footer>
---
+++ header.php
@@ -0,0 +1,6 @@
+<button class="c-slide--button"><a>Menu</a></button><!-- メニュー表示ボタン -->
+      <header class="p-header l-header">
+        <P class="c-logo">Hamburger</P>
+        <?php get_search_form(); ?><!-- サーチフォームの呼び出し -->
+        <div class="c-color-board--black"></div><!-- メニュー展開時に画面を暗くする -->
+      </header>
