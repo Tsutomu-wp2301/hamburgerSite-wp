f295898dcf53a8a3fbb18e2dba965cf06ed0472c
Tsutomu <tutokun0723@gmail.com>
2023-04-23 22:48:32 +0900

    menu作成途中

--- page.php
+++ page.php
@@ -99,53 +99,6 @@
         </article>
       </section>
     </main>
-    <aside class="p-aside p-aside--fonts l-aside l-aside__font-layout">
-      <button class="c-button-close"><p>Menuを閉じる</p></button><!-- メニュー閉じるボタン -->
-      <h2>Menu</h2>
-      <h3>バーガー</h3>
-      <ul>
-        <li>ハンバーガー</li>
-        <li>チーズバーガー</li>
-        <li>テリヤキバーガー</li>
-        <li>アボカドバーガー</li>
-        <li>フィッシュバーガー</li>
-        <li>ベーコンバーガー</li>
-        <li>チキンバーガー</li>
-      </ul>
-      <h3>サイド</h3>
-      <ul>
-        <li>ポテト</li>
-        <li>サラダ</li>
-        <li>ナゲット</li>
-        <li>コーン</li>
-      </ul>
-      <h3>ドリンク</h3>
-      <ul>
-        <li>コーラ</li>
-        <li>ファンタ</li>
-        <li>オレンジ</li>
-        <li>アップル</li>
-        <li>紅茶(Ice/Hot)</li>
-        <li>コーヒー(Ice/Hot)</li>
-        <li>コーラ</li>
-        <li>ファンタ</li>
-        <li>オレンジ</li>
-        <li>アップル</li>
-        <li>紅茶(Ice/Hot)</li>
-        <li>コーヒー(Ice/Hot)</li>
-        <li>コーラ</li>
-        <li>ファンタ</li>
-        <li>オレンジ</li>
-        <li>アップル</li>
-        <li>紅茶(Ice/Hot)</li>
-        <li>コーヒー(Ice/Hot)</li>
-        <li>コーラ</li>
-        <li>ファンタ</li>
-        <li>オレンジ</li>
-        <li>アップル</li>
-        <li>紅茶(Ice/Hot)</li>
-        <li>コーヒー(Ice/Hot)</li>
-      </ul>
-    </aside>
+    <?php get_sidebar(); ?>
   </div>
   <?php get_footer(); ?>
