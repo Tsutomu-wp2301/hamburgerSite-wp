d56542512e4e72ad4105100ac67a18ebe05745ca
Tsutomu <tutokun0723@gmail.com>
2023-05-15 01:05:23 +0900

    pagenvi

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
-<form class="p-search" method="get" action="<?php echo home_url('/'); ?>">
-  <input type="search" class="c-search-area" placeholder="キーワード" name="S" id="s">
+<form class="p-search" method="get" action="<?php echo esc_url(home_url('/')); ?>">
+  <input type="search" class="c-search-area" placeholder="<?php if(!is_search()){ echo 'キーワード';} ?>" value="<?php if(is_search()){ echo esc_attr(get_search_query());} ?>" name="s" id="s">
   <button class="c-button" type="submit" value="検索">検索</button>
   <div class="c-search--icon"></div>
 </form>
