ed6a920ff5dcffd5567761737b1e95efb755bccb
Tsutomu <tutokun0723@gmail.com>
2023-05-24 00:00:33 +0900

    404ページの内容を追記、シングルページと固定ページのメインループ内コンテンツを追加

--- page-shop.php
+++ page-shop.php
@@ -1,35 +1,15 @@
-<?php get_header(); ?><!-- ヘッダーの呼び出し -->
-
 <?php 
 /* 
 Template Name:ショップについて
 */ 
 ?>
+
+<?php get_header(); ?><!-- ヘッダーの呼び出し -->
+
+
       <section>
         <h1 class="p-page-image"><?php the_title(); ?></h1>
         <article class="p-single--layout">
-          <h2><?php echo esc_html(CFS()->get('h2')); ?></h2>
-          <p><?php echo esc_html(CFS()->get('h2-text')); ?></p>
-          <h3><?php echo esc_html(CFS()->get('h3')); ?></h3>
-          <h4><?php echo esc_html(CFS()->get('h4')); ?></h4>
-          <h5><?php echo esc_html(CFS()->get('h5')); ?></h5>
-          <h6><?php echo esc_html(CFS()->get('h6')); ?></h6>
-     
-          <blockquote>
-            blockquote引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用<br>
-            <p>出典元：　<a href="#">○○○○○○○○○○○○</a></p>
-          </blockquote>
-          <article class="p-flex--single">
-            <img class="p-product--image--long" src="<?php echo esc_html(CFS()->get('main-menu-image')); ?>">
-            <div class="p-flex--single-content"><!-- 写真とテキストを横並び -->
-              <img class="p-product--image" src="<?php echo esc_html(CFS()->get('menu1--image')); ?>">
-              <P><?php echo esc_html(CFS()->get('product-text1')); ?></P>
-            </div>
-            <div class="p-flex--single-content"><!-- 写真とテキストを横並び -->
-              <img class="p-product--image" src="<?php echo esc_html(CFS()->get('menu2--image')); ?>">
-              <P class="left"><?php echo esc_html(CFS()->get('product-text2')); ?></P>
-            </div>
-          </article>
           <?php if( have_posts() ) :  while( have_posts() ) : the_post(); ?><!-- メインループ -->
             <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
               <?php the_content(); ?>
--- single.php
+++ single.php
@@ -8,40 +8,7 @@
         <?php if( have_posts() ) :  while( have_posts() ) : the_post(); ?>
           <h1 class="p-single-image"><?php echo get_the_title(); ?></h1>
           <article class="p-single--layout">
-            <h2>
-              <?php echo esc_html(CFS()->get('h2-menu')); ?>
-            </h2>
-            <p>
-              <?php echo esc_html(CFS()->get('h2-menu-text')); ?>
-            </p>
-            <h3>
-              <?php echo esc_html(CFS()->get('h3-menu')); ?>
-            </h3>
-            <h4>
-              <?php echo esc_html(CFS()->get('h4-menu')); ?>
-            </h4>
-            <h5>
-              <?php echo esc_html(CFS()->get('h5-menu')); ?>
-            </h5>
-            <h6>
-              <?php echo esc_html(CFS()->get('h6-menu')); ?>
-            </h6>
-            <blockquote>
-              blockquote引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用<br>
-              <p>出典元：　<a href="#">○○○○○○○○○○○○</a></p>
-            </blockquote>
-            <article class="p-flex--single">
-                <img class="p-product--image--long" src="<?php echo esc_html(CFS()->get('menu-image')); ?>">
-              <div class="p-flex--single-content"><!-- 写真とテキストを横並び -->
-                <img class="p-product--image" src="<?php echo esc_html(CFS()->get('menu1-image')); ?>">
-                <P><?php echo esc_html(CFS()->get('menu1-text')); ?></P>
-              </div>
-              <div class="p-flex--single-content"><!-- 写真とテキストを横並び -->
-                <img class="p-product--image" src="<?php echo esc_html(CFS()->get('menu2-image')); ?>">
-                <P class="left"><?php echo esc_html(CFS()->get('menu2-text')); ?></P>
-              </div>
-              <?php the_content(); ?>
-            </article>
+            <?php the_content(); ?>
           </article>
         <?php endwhile;  endif; ?>
       </section>
